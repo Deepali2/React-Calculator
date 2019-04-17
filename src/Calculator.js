5781141e472b1d40f8ba1df2e8060add712a53a2
Deepali2 <hdgarg@gmail.com>
2019-04-17 11:57:45 -0700

    adds signature

---
+++ src/Calculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="App">
+        CALCULATOR
+        <h5>Designed and Maintained by deepaligarg1001@gmail.com</h5>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
