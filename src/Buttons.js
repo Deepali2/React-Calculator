ca6e585f37f2e74cd491c270c87433d1e55701da
Deepali2 <hdgarg@gmail.com>
2019-04-17 14:44:37 -0700

    adds styling for the NumberButton

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Buttons.css';
 import EqualsButton from './EqualsButton';
+import NumberButton from './numberButton';
 
 const Buttons = () => {
   return (
     <div className='Buttons'>
+      <NumberButton />
       <EqualsButton />
     </div>
   );
---
+++ src/numberButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './numberButton.css';
+
+const NumberButton = () => {
+  return (
+    <div className='NumberButton'></div>
+  );
+};
+
+export default NumberButton;
+
+
