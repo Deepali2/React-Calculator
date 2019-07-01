b6985e0dab929dbaf411f0c967c8140dc7039788
Deepali2 <hdgarg@gmail.com>
2019-07-01 00:45:58 -0700

    updated the README

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import './Calculator.css';
-import Input from './Input.js';
-import Buttons from './Buttons.js';
-import operations from './operations.js';
-
-
+import React, { Component } from "react";
+import "./Calculator.css";
+import Input from "./Input.js";
+import Buttons from "./Buttons.js";
+import operations from "./operations.js";
 
 class Calculator extends Component {
   constructor() {
     super();
-    this.state= {
+    this.state = {
       resultDisplayed: false,
-      input:''
+      input: ""
     };
     this.clear = this.clear.bind(this);
     this.equalsPressed = this.equalsPressed.bind(this);
@@ -21,12 +19,12 @@ class Calculator extends Component {
 
   clear = () => {
     this.setState({
-      input: '',
+      input: "",
       resultDisplayed: false
     });
-  }
+  };
 
-  numberButtonClick = (e) => {
+  numberButtonClick = e => {
     let currentString = this.state.input;
     const operationChars = Object.keys(operations);
     let lastChar = currentString[currentString.length - 1];
@@ -36,22 +34,25 @@ class Calculator extends Component {
         input: currentString
       });
       //if the result Displayed is true and if the user pressed an operation key
-    } else if (this.state.resultDisplayed === true && operationChars.indexOf(lastChar)!== -1) {
+    } else if (
+      this.state.resultDisplayed === true &&
+      operationChars.indexOf(lastChar) !== -1
+    ) {
       currentString += e.target.value;
       this.setState({
         resultDisplayed: false,
         input: currentString
-      }) //we need to keep adding to the string
+      }); //we need to keep adding to the string
     } else {
       this.setState({
         resultDisplayed: false,
         input: e.target.value
-      })
+      });
     }
-  }
+  };
 
-  operatorButtonClick = (e) => {
-    //if the input string is empty then return 
+  operatorButtonClick = e => {
+    //if the input string is empty then return
     let inputString = this.state.input;
     if (inputString.length === 0) return;
 
@@ -59,72 +60,74 @@ class Calculator extends Component {
     let lastCharac = inputString[inputString.length - 1];
     let operationsKeys = Object.keys(operations);
     if (operationsKeys.indexOf(lastCharac) !== -1) {
-      let newString = inputString.substring(0, inputString.length - 1) + e.target.value;
+      let newString =
+        inputString.substring(0, inputString.length - 1) + e.target.value;
       this.setState({
         input: newString
-      })
+      });
       //else add the operator to the input string
     } else {
       let newString = this.state.input + e.target.value;
       this.setState({
         input: newString
-      })
-    }    
-  }
-
- 
+      });
+    }
+  };
 
   equalsPressed = () => {
-    if(this.state.input === '') return;
+    if (this.state.input === "") return;
     let inputString = this.state.input;
 
     //if the input is just a number then pressing equals does nothing
-    if(Number(inputString)) return;
-    
+    if (Number(inputString)) return;
+
     //else figure out the answer
-    let numbers = inputString.split(/\+|-|×|÷/g);//array of numbers
-    let operators = inputString.replace(/[0-9]|\./g, '').split('');//array of operators    
+    let numbers = inputString.split(/\+|-|×|÷/g); //array of numbers
+    let operators = inputString.replace(/[0-9]|\./g, "").split(""); //array of operators
     const operationChars = Object.keys(operations); //[÷, ×, +, -]
 
     for (let i = 0; i < operationChars.length; i++) {
-      let currentOperator = operationChars[i];  //will start with ÷
+      let currentOperator = operationChars[i]; //will start with ÷
       let currentOperationFunction = operations[currentOperator]; //get the function associated with the operator
       let indexOfOperationToExecute = operators.indexOf(currentOperator);
-      while(indexOfOperationToExecute !== -1) {
-        let currentResult = currentOperationFunction(numbers[indexOfOperationToExecute], numbers[indexOfOperationToExecute + 1]);
+      while (indexOfOperationToExecute !== -1) {
+        let currentResult = currentOperationFunction(
+          numbers[indexOfOperationToExecute],
+          numbers[indexOfOperationToExecute + 1]
+        );
         numbers.splice(indexOfOperationToExecute, 2, currentResult);
         operators.splice(indexOfOperationToExecute, 1);
         indexOfOperationToExecute = operators.indexOf(currentOperator);
-      }       
+      }
     }
-    
+
     // let result = numbers[0]; //it is a string
-    let result = '' + numbers[0];
+    let result = "" + numbers[0];
     //if the result is a long decimal
     let MAXLENGTH = 13;
     if (result.length > MAXLENGTH) {
       //show the rounded value so length of string displayed is 13
-      let truncatedResult = result.substring(0, MAXLENGTH + 2) //get till the 14th place
+      let truncatedResult = result.substring(0, MAXLENGTH + 2); //get till the 14th place
       if (Number(truncatedResult[truncatedResult.length - 1]) >= 5) {
         let lastDigit = Number(truncatedResult[truncatedResult.length - 1]);
         lastDigit++;
         result = truncatedResult.substring(0, MAXLENGTH - 1) + lastDigit;
       }
-    }    
+    }
     //set the state
     this.setState({
       input: result,
       resultDisplayed: true
     });
-  }
-  
+  };
+
   render() {
     return (
       <div className="App">
         <h1>CALCULATOR</h1>
-        <div className='Calculator'>
-          <Input input={this.state.input}/>
-          <Buttons 
+        <div className="Calculator">
+          <Input input={this.state.input} />
+          <Buttons
             numberButtonClick={this.numberButtonClick}
             operatorButtonClick={this.operatorButtonClick}
             equalsPressed={this.equalsPressed}
